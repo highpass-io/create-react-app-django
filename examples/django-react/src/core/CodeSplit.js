9cf16dcd6bc9f89ef245299a6388174951a35828
Yanglin Zhao <yz1876@gmail.com>
2017-09-24 00:27:50 -0400

    Add example for code-splitting

---
+++ examples/django-react/src/core/CodeSplit.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CodeSplit extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Code Split Example</h1>
+        <p>This component is loaded dynamically</p>
+      </div>
+    );
+  }
+}
+
+export default CodeSplit;
